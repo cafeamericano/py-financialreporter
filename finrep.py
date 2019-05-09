5b6128dc8688a896ce1c3a44835ddacdb94f19c8
Developer <Developer@Matthews-MacBook-Air.local>
2019-05-08 23:33:19 -0400

    Expanded to allow for variable numbers of assets and liabilities

--- finrep.py
+++ finrep.py
@@ -3,9 +3,11 @@
 #Welcome
 print('\n')
 print("*** Welcome to the Financial Report Machine - Python Edition *** ")
+print('\n')
 enter = raw_input("Press Enter to continue... ")
 
 #Get date
+print('\n')
 enteredDate = raw_input("What is today's date? ")
 with open('report.txt', 'a') as file:
     file.write('\n')
@@ -21,17 +23,25 @@ with open('report.txt', 'a') as file:
     file.write('\n')
 
 #Get asset information
-print("Got it. Now, let's gather information about your assets. ")
-enter = raw_input("Press Enter to continue... ")
-asset1Name = raw_input("What is the name of the asset? ")
-with open('report.txt', 'a') as file:
-    file.write(str(asset1Name))
-    file.write(": ")
+print('\n')
+enter = raw_input("Got it. Now, let's gather information about your assets. Press Enter to continue... ")
 
-asset1Value = input("What is the value of the asset? ")
-with open('report.txt', 'a') as file:
-    file.write(str(asset1Value))
-    file.write('\n')
+enter = raw_input("Would you like to add an asset? Press Enter to do so, or type 'next' and press Enter to continue. ")
+print('\n')
+
+assetAccum = 0
+while enter != "next":
+    assetName = raw_input("What is the name of the asset? ")
+    with open('report.txt', 'a') as file:
+        file.write(str(assetName))
+        file.write(": ")
+    assetValue = input("What is the value of the asset? ")
+    assetAccum = assetAccum + assetValue
+    with open('report.txt', 'a') as file:
+        file.write(str(assetValue))
+        file.write('\n')
+    enter = raw_input("Would you like to add another asset? Press Enter to do so, or type 'next' and press Enter to continue. ")
+    print('\n')
 
 ####################################################################################
 
@@ -42,17 +52,25 @@ with open('report.txt', 'a') as file:
     file.write('\n')
 
 #Get liability information
-print("Got it. Now, let's gather information about your liabilities. ")
-enter = raw_input("Press Enter to continue... ")
-liability1Name = raw_input("What is the name of the liabilities? ")
-with open('report.txt', 'a') as file:
-    file.write(str(liability1Name))
-    file.write(": ")
+print('\n')
+enter = raw_input("Ok. Now, let's gather information about your liabilities. Press Enter to continue... ")
 
-liability1Value = input("What is the value of the liabilities? ")
-with open('report.txt', 'a') as file:
-    file.write(str(liability1Value))
-    file.write('\n')
+enter = raw_input("Would you like to add a liability? Press Enter to do so, or type 'next' and press Enter to continue. ")
+print('\n')
+
+liabilityAccum = 0
+while enter != "next":
+    liabilityName = raw_input("What is the name of the liability? ")
+    with open('report.txt', 'a') as file:
+        file.write(str(liabilityName))
+        file.write(": ")
+    liabilityValue = input("What is the value of the liability? ")
+    liabilityAccum = liabilityAccum + liabilityValue
+    with open('report.txt', 'a') as file:
+        file.write(str(liabilityValue))
+        file.write('\n')
+    enter = raw_input("Would you like to add another liability? Press Enter to do so, or type 'next' and press Enter to continue. ")
+    print('\n')
 
 ####################################################################################
 
@@ -62,19 +80,18 @@ with open('report.txt', 'a') as file:
     file.write("***NET WORTH***")
     file.write('\n')
 
-netWorth = asset1Value - liability1Value
+netWorth = assetAccum - liabilityAccum
 
 with open('report.txt', 'a') as file:
     
     file.write("Total Assets: ")
-    file.write(str(asset1Value))
+    file.write(str(assetAccum))
     file.write('\n')
 
     file.write("Total Liabilities: ")
-    file.write(str(liability1Value))
+    file.write(str(liabilityAccum))
     file.write('\n')
 
-    file.write('\n')
     file.write("Net Worth: ")
     file.write(str(netWorth))
     file.write('\n')
@@ -82,5 +99,9 @@ with open('report.txt', 'a') as file:
 ####################################################################################
 
 #Exit program
-enter = raw_input("Press Enter to terminate the program... ")
+print("Your total assets are " + str(assetAccum))
+print("Your total liabilities are " + str(liabilityAccum))
+print("Your net worth is " + str(netWorth))
+print('\n')
+enter = raw_input("A report has been generated using the information provided. Press Enter to terminate the program... ")
 
